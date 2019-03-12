fc15e75518d516be123a8d0faa2978e058eb5aac
giacomo.casoni <giacomo.casoni@target-holding.nl>
2019-03-12 16:01:17 +0100

    Changed the urls

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -13,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title='My API title'))
 ]
